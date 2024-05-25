2f2697e88aa8d61e0a0716dfb586a7ec55bf7c84
barakadax <barakadax@gmail.com>
2024-05-25 22:05:23 +0300

    added cpp and reorder directories and added more information to readme

---
+++ cpp/write.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string>
+#include <locale>
+#include <codecvt>
+
+using namespace std;
+
+int main() {
+    string portName = "/dev/pts/8";
+    int serialPort = open(portName.c_str(), O_WRONLY | O_NOCTTY);
+
+    if (serialPort == -1) {
+        cerr << "Error opening serial port: " << portName << endl;
+        return 1;
+    }
+
+    termios tty;
+    tcgetattr(serialPort, &tty);
+
+    cfsetispeed(&tty, B115200);
+    cfsetospeed(&tty, B115200);
+
+    tty.c_cflag &= ~PARENB;
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CSIZE;
+    tty.c_cflag |= CS8;
+    tty.c_cflag &= ~CRTSCTS;
+
+    tty.c_cc[VMIN] = 1;
+    tty.c_cc[VTIME] = 0;
+
+    tcsetattr(serialPort, TCSANOW, &tty);
+
+    while (true)
+    {
+        cout << "Enter a message to send: ";
+        string dataToWrite;
+        getline(cin, dataToWrite);
+
+        wstring_convert<codecvt_utf8_utf16<wchar_t>> converter;
+        wstring utf16Data = converter.from_bytes(dataToWrite);
+
+        int bytesWritten = write(serialPort, utf16Data.c_str(), utf16Data.size() * sizeof(wchar_t));
+
+        if (bytesWritten == -1) {
+            cerr << "Error writing to serial port" << endl;
+        }
+    }
+    
+    close(serialPort);
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -10,12 +10,18 @@ pip install pyserial
 pip install pyserial-asyncio
 ```
 
+## How to compile C++:
+```shell
+g++ -o a write.cpp
+```
+
 ## How to run:
 ### 1:
 Create bidirectional socket cable communication:
 ```shell
 socat -d -d pty,raw,echo=0 pty,raw,echo=0
 ```
+It will create 2 address, when writing to any of those you can read from the other.
 
 ### 2:
 Run any of the readers.
